d9e5a5fdef33bdded9ba438059d0116d3eff504b
joaopedroprogram <joaopedrosouzarodriguesj@gmail.com>
2025-09-14 01:30:10 -0300

    terminado js

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             navbar.style.backgroundColor = 'rgba(10,10,10,0.98)':
             navbar.style.backgroundColor = 'rgba(10,10,10,0.95)';
     });
+function enviarWhats(event){
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value
+    const telefone = '5585986557364'
+    const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto)
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    console.log(url)
+
+    window.open(url, '_blank')
+
+}
