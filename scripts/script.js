58be0cb9a5cb58d737501fe7073263d69b61d621
joaopedroprogram <joaopedrosouzarodriguesj@gmail.com>
2025-09-14 03:00:26 -0300

    comitando

--- scripts/script.js
+++ scripts/script.js
@@ -20,9 +20,6 @@ function enviarWhats(event){
     const texto = `Olá! Me chamo ${nome}, ${mensagem}`;
     const msgFormatada = encodeURIComponent(texto)
     const url = `https://wa.me/${telefone}?text=${msgFormatada}`
-
-    console.log(url)
-
     window.open(url, '_blank')
 
 }
